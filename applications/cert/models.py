e584f83e42279b517e79b5693ae6629970f85b5d
o-ddong <odh0112@naver.com>
2022-10-01 22:39:16 +0900

    Fix: 누락된 AbstractBaseUser 기입

--- applications/cert/models.py
+++ applications/cert/models.py
@@ -1,22 +1,22 @@
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import UserManager
 from django.db import models
-
-# Create your models here.
 from applications.base.models import BaseModel
 
-<<<<<<< HEAD
+
+
 """ 유저 관리 테이블 """
-=======
+class User(AbstractBaseUser, BaseModel):
+    objects = UserManager()
 
->>>>>>> fa7e7de759d79059af8920d620a2bb2154b9a546
-class User(BaseModel):
     id = models.BigAutoField(primary_key=True, verbose_name=("회원 번호"))
-    email = models.CharField(max_length=255, null=True, verbose_name=("이메일"))
-    password = models.CharField(max_length=100, null=True, verbose_name=("비밀 번호"))
-    name = models.CharField(max_length=20, null=True, verbose_name=("사용자 이름"))
-    mdn = models.CharField(max_length=100, null=True, verbose_name=("전화 번호"))
-    grade = models.CharField(max_length=15, null=True, verbose_name=("회원 등급"))
-    is_admin = models.BooleanField(verbose_name=("관리자 여부"))
+    email = models.EmailField(max_length=255, unique=True, null=False, verbose_name=("이메일"))
+    password = models.CharField(max_length=100, null=False, verbose_name=("비밀 번호"))
+    name = models.CharField(max_length=20, null=True, default=None, verbose_name=("사용자 이름"))
+    mdn = models.CharField(max_length=100, null=True, default=None, verbose_name=("전화 번호"))
+    grade = models.CharField(max_length=15, null=True, default=None, verbose_name=("회원 등급"))
+    is_admin = models.BooleanField(default=False, verbose_name=("관리자 여부"))
 
-
-
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['password']
 
