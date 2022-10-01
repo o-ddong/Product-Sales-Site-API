34575dac5f923459d72788607a49f20e99c8ab5f
o-ddong <odh0112@naver.com>
2022-10-01 22:40:01 +0900

    Feat: 회원 조회, 생성, 수정, 삭제 기능 구현

--- applications/cert/views.py
+++ applications/cert/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import mixins, generics
+
+from applications.cert.models import User
+from applications.cert.serializer import UserCreateListSerializer, UserDetailSerializer
+
+
+class UserCreateListMixins(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreateListSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request)
+
+
+class UserDetailMixins(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserDetailSerializer
+
+    def get(self, request, pk, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
