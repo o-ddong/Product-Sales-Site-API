479c6df736ad1330d404de8104be013f187ceded
o-ddong <odh0112@naver.com>
2022-10-02 00:54:36 +0900

    Feat: 상품 관리 path 설정

--- applications/billing/urls.py
+++ applications/billing/urls.py
@@ -13,6 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
+from applications.billing.views import ProductCreateListMixins, ProductDetailMixins
+
+urlpatterns = [
+    path('product/', ProductCreateListMixins.as_view()),
+    path('product/<int:pk>', ProductDetailMixins.as_view()),
+]
