13510adf61f890ecacc6b46614963e9a8ef5c59d
o-ddong <odh0112@naver.com>
2022-10-02 01:03:27 +0900

    Feat: 결제 내역 조회, 생생, 수정, 삭제 serializer 구현

--- applications/billing/serializer.py
+++ applications/billing/serializer.py
@@ -4,13 +4,33 @@ from applications.cert.models import User
 
 
 class ProductCreateListSerializer(serializers.ModelSerializer):
-
+    """
+    상품 생성 및 조회
+    """
     class Meta:
         model = User
         fields = "__all__"
 
 class ProductDetailSerializer(serializers.ModelSerializer):
+    """
+    상품 상세 조회, 수정, 삭제
+    """
+    class Meta:
+        model = User
+        fields = "__all__"
 
+class PaymentCreateListSerializer(serializers.ModelSerializer):
+    """
+    결제 생성 및 조회
+    """
+    class Meta:
+        model = User
+        fields = "__all__"
+
+class PaymentDetailSerializer(serializers.ModelSerializer):
+    """
+    결제 상세 조회, 수정, 삭제
+    """
     class Meta:
         model = User
         fields = "__all__"
