237ffd017bf6c1ff7a854f61cce70f02ed72fe33
o-ddong <odh0112@naver.com>
2022-10-02 11:45:05 +0900

    Fix: DB 삭제 시 response 수정

--- applications/base/response.py
+++ applications/base/response.py
@@ -11,7 +11,7 @@ operation_failure = Response(status=400, data={"message": "OPERATION_FAILURE"})
 operation_success = Response(status=200, data={"message": "OPERATION_SUCCESS"})
 
 # DB 정상 삭제
-operation_deleted = Response(status=200, data={"message": "OPERATION_SUCCESS"})
+operation_deleted = Response(status=200, data={"message": "DATA_DELETED_SUCCESS"})
 
 # 예외 처리 billing
 certification_failure = Response(data={'message': 'NOT_ADMINISTRATOR_FAILURE', "status_code": 4101})
