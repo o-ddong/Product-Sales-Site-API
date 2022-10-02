6962effff3861f2e3ec3f39ba0ae3326218e1dde
o-ddong <odh0112@naver.com>
2022-10-02 11:59:46 +0900

    Style: serializer 주석 추가

--- applications/cert/serializer.py
+++ applications/cert/serializer.py
@@ -4,13 +4,17 @@ from applications.cert.models import User
 
 
 class UserCreateListSerializer(serializers.ModelSerializer):
-
+    """
+    유저 조회 및 생성
+    """
     class Meta:
         model = User
         exclude = ['last_login', 'grade','created_at', 'updated_at']
 
 class UserDetailSerializer(serializers.ModelSerializer):
-
+    """
+    유저 상세 조회, 수정, 삭제
+    """
     class Meta:
         model = User
         fields = "__all__"
